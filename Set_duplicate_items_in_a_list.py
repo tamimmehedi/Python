e2409dcdc3cf0a868b86296a2042a98d0b4e6ab9
MD Tamim Mehedi <43099464+tamimmehedi@users.noreply.github.com>
2022-01-24 15:52:35 -0500

    Create Set_duplicate_items_in_a_list.py

---
+++ Set_duplicate_items_in_a_list.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+l1 = [1,2,1,2,3,4,5,1,1,2,5,6,7,8,9,9]
+d = Counter(l1)
+print(d)
+
+new_list = set([item for item in d if d[item]>1])
+print(new_list)
+
+/////////////
+
+from collections import Counter
+
+l1 = [1,2,1,2,3,4,5,1,1,2,5,6,7,8,9,9]
+d = Counter(l1)
+print(d)
+
+new_list = list([item for item in d if d[item]>1])
+print(new_list)
